f69809f122755357a2f4ed74cf3f8e0903082a56
hjw4595 <hjw4595@naver.com>
2019-07-27 22:14:48 +0900

    apollo set-up

---
+++ react-note/src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { ApolloProvider } from "react-apollo";
+import client from "./apollo";
+import GlobalStyle from "./globalStyles";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+      <GlobalStyle>
+          
+      </GlobalStyle>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);

a425a5c1e30e3e14a0a4a0e1d60e3f15642c7cca
hjw4595 <hjw4595@naver.com>
2019-07-28 18:45:46 +0900

    Quert and fragments

--- react-note/src/App.js
+++ react-note/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+// import { Query } from 'react-apollo';
+// import {GET_NOTE} from "./queries"
 
 
 function App() {
   return (
     <div className="App">
+      {/* <Query query={GET_NOTE}>{() => null}</Query> */}
     </div>
   );
 }
